7ded63a42a9a2a34c8a98673ef32453fecae175e
Smith <z@gmail.com>
2016-06-17 05:19:07 +0200

    v1.0.0
    - updated SDK + build tools + appCompat support lib
    - kukorica API
    - paysskey shared pref.

---
+++ base/src/main/java/butter/droid/base/fragments/dialog/StringInputDialogFragment.java
@@ -0,0 +1,94 @@
+/*
+ * This file is part of Butter.
+ *
+ * Butter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Butter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Butter. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package butter.droid.base.fragments.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.InputType;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import butter.droid.base.R;
+
+
+public class StringInputDialogFragment extends DialogFragment {
+
+    public static final String TITLE = "title";
+    public static final String DEFAULT_VALUE = "default_val";
+
+    private ResultListener mOnResultListener;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        if (getArguments() == null || !getArguments().containsKey(TITLE) || mOnResultListener == null) {
+            return builder.create();
+        }
+
+        final String currentValue = getArguments().getString(DEFAULT_VALUE, "");
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        final EditText stringInput = new EditText(getActivity());
+        stringInput.setLayoutParams(lp);
+        stringInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+        stringInput.setText(currentValue);
+
+        builder
+                .setView(stringInput)
+                .setTitle(getArguments().getString(TITLE))
+                .setPositiveButton(R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mOnResultListener.onNewValue(stringInput.getText().toString());
+                                dialog.dismiss();
+                            }
+                        })
+                .setNegativeButton(R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }
+                );
+
+        return builder.create();
+    }
+
+    public void setOnResultListener(ResultListener resultListener) {
+        mOnResultListener = resultListener;
+    }
+
+    public interface ResultListener {
+        public void onNewValue(String value);
+    }
+
+}
